bde6a2ef0de9f917433311e742aee1a1326fbcf1
NightFarmer <494090597@qq.com>
2016-01-08 12:57:10 +0800

    新增Fragment中调用扫描解码

--- zxing/src/main/java/com/nightfarmer/zxing/ScanHelper.java
+++ zxing/src/main/java/com/nightfarmer/zxing/ScanHelper.java
@@ -3,6 +3,7 @@ package com.nightfarmer.zxing;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.support.v4.app.Fragment;
 
 import com.google.zxing.WriterException;
 import com.nightfarmer.zxing.activity.CaptureActivity;
@@ -13,11 +14,16 @@ import com.nightfarmer.zxing.encoding.EncodingHandler;
  * Created by zhangfan on 16-1-8.
  */
 public class ScanHelper {
-    public static final int RequestCode = 1;
+    public static final int RequestCode = 10086;
 
     public static void capture(Activity activity) {
         Intent intent = new Intent(activity, CaptureActivity.class);
         activity.startActivityForResult(intent, RequestCode);
+    }
+
+    public static void capture(Fragment fragment) {
+        Intent intent = new Intent(fragment.getContext(), CaptureActivity.class);
+        fragment.startActivityForResult(intent, RequestCode);
     }
 
     public static String handlerData(int requestCode, int resultCode, Intent data) {
